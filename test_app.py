cb7c6e062cd4209578818604c7b49fe056844864
sgoren1 <44706709+sgoren1@users.noreply.github.com>
2025-03-19 13:40:30 +0100

    maj du test

--- test_app.py
+++ test_app.py
@@ -1,5 +1,12 @@
 from main import add
+import pytest
 
-def test_add():
-    assert add(2,3) == 5
-
+@pytest.mark.parametrize("a, b, expected", [
+    (2, 3, 5),
+    (-1, 1, 0),
+    (0, 0, 0),
+    (1.5, 2.5, 4.0),
+    (-2, -3, -5)
+])
+def test_add(a, b, expected):
+    assert add(a, b) == expected
